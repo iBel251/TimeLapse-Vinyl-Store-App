bc9ee4737595bde5fafdd6d8f297f252e259a114
Imran <Ibbimran7@gmail.com>
2025-12-16 14:24:06 -0800

    First commit

---
+++ capstone-api-starter/src/main/java/org/yearup/TimeLapseStoreApplication.java
@@ -0,0 +1,14 @@
+package org.yearup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeLapseStoreApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeLapseStoreApplication.class, args);
+    }
+
+}
