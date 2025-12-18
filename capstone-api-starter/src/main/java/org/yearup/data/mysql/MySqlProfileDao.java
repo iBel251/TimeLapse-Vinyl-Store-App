a37827cca1753897ccc6cd622e37beb035a08a65
Imran <Ibbimran7@gmail.com>
2025-12-17 20:15:45 -0800

    - Min and Max price filters bug fixed. - Profile get and update endpoints fixed.

--- capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -16,6 +16,34 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
     }
 
     @Override
+    public Profile getByUserId(int userId)
+    {
+        String sql = "SELECT user_id, first_name, last_name, email, phone, " +
+                "       address, city, state, zip " +
+                "FROM profiles WHERE user_id = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql))
+        {
+            statement.setInt(1, userId);
+
+            try (ResultSet rs = statement.executeQuery())
+            {
+                if (rs.next())
+                {
+                    return mapRow(rs);
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
+
+    @Override
     public Profile create(Profile profile)
     {
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
@@ -43,5 +71,58 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
             throw new RuntimeException(e);
         }
     }
+
+    @Override
+    public void update(int userId, Profile profile)
+    {
+        String sql = "UPDATE profiles " +
+                "SET first_name = ?, " +
+                "    last_name  = ?, " +
+                "    email      = ?, " +
+                "    phone      = ?, " +
+                "    address    = ?, " +
+                "    city       = ?, " +
+                "    state      = ?, " +
+                "    zip        = ? " +
+                "WHERE user_id = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql))
+        {
+            statement.setString(1, profile.getFirstName());
+            statement.setString(2, profile.getLastName());
+            statement.setString(3, profile.getEmail());
+            statement.setString(4, profile.getPhone());
+            statement.setString(5, profile.getAddress());
+            statement.setString(6, profile.getCity());
+            statement.setString(7, profile.getState());
+            statement.setString(8, profile.getZip());
+            statement.setInt(9, userId);
+
+            statement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Profile mapRow(ResultSet rs) throws SQLException
+    {
+        Profile profile = new Profile();
+        profile.setUserId(rs.getInt("user_id"));
+        profile.setFirstName(rs.getString("first_name"));
+        profile.setLastName(rs.getString("last_name"));
+        profile.setEmail(rs.getString("email"));
+        profile.setPhone(rs.getString("phone"));
+        profile.setAddress(rs.getString("address"));
+        profile.setCity(rs.getString("city"));
+        profile.setState(rs.getString("state"));
+        profile.setZip(rs.getString("zip"));
+
+        return profile;
+    }
+
+
 
 }
